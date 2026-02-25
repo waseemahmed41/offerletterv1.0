72420d2bcedaefef1c8de9294944b49d08ef82c1
waseemahmed41 <mdwaseemahmed156@gmail.com>
2026-02-25 23:13:04 +0530

    Fix Vercel serverless function handler format

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-from django.core.asgi import get_asgi_application
 
 # Add project root to Python path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
@@ -9,5 +8,12 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 # Set Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'offer_automation.settings')
 
-# Get WSGI application for Vercel
-application = get_wsgi_application()
+# Get Django application
+django_app = get_wsgi_application()
+
+# Vercel serverless function handler
+def handler(request):
+    return django_app(request)
+
+# Export for Vercel
+app = handler
